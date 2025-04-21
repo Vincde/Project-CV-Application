11e3ae06e9d0f8d7bc351043ae039d37f9c49e04
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-21 15:29:49 +0200

    feat:Add general info component file

---
+++ src/GeneralInfo.jsx
@@ -0,0 +1,22 @@
+function GeneralInfo() {
+
+    return(
+        <div>
+            <div>
+                <h2>Name:</h2>
+                <input type="text" />
+            </div>
+            <div>
+                <h2>Name:</h2>
+                <input type="text" />
+            </div>
+            <div>
+                <h2>Name:</h2>
+                <input type="text" />
+            </div>
+        </div>
+        );
+}
+
+
+export default GeneralInfo;
