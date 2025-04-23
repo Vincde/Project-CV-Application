37904f04757a17cff338b302352663f64f65260a
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-23 10:31:02 +0200

    &&

--- src/GeneralInfo.jsx
+++ src/GeneralInfo.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
+
 function GeneralInfo() {
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [phoneNumber,setPhoneNumber] = useState();
+
 
     return(
         <div>
             <div>
-                <h2>Name:</h2>
-                <input type="text" />
+                <p>Name:</p>
+                <input type="text" onChange={(e) => setName(e.target.value)} />
             </div>
             <div>
-                <h2>Name:</h2>
-                <input type="text" />
+                <p>Email:</p>
+                <input type="text" onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div>
-                <h2>Name:</h2>
-                <input type="text" />
+                <p>Phone number:</p>
+                <input type="number" onChange={(e) => setPhoneNumber(e.target.value)} />
             </div>
         </div>
         );
 }
+
 
 
 export default GeneralInfo;
