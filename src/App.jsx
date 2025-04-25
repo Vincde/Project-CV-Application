df3b4ce45d7b9ae59bb12b923aca87db3f24068d
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-25 13:05:16 +0200

    feat: Add boilerplate for all the inputs of educational experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import GeneralInfo from './GeneralInfo'
-
+import EducationalExperience from './Education';
 import './App.css'
 import { useState } from 'react'
 
 function App() {
 
   const [generalInfo, setGeneralInfo] = useState({name:'',email:'',phone:''});
+  const [education, setEducation] = useState({schoolName: '', titleStudy: '', dateStudy: ''});
 
   return(
     <>
     <div>
       <GeneralInfo info={generalInfo} setInfo={setGeneralInfo}></GeneralInfo>
+      <EducationalExperience education={education} setEducation={setEducation}></EducationalExperience>
     </div>
     </>
   )
---
+++ src/Education.jsx
@@ -0,0 +1,40 @@
+import './education.css';
+
+function EducationalExperience(education, setEducation) {
+    function handleInputChange(type, e){
+        let newEducation = {};
+
+        if(type === 'schoolName'){
+            newEducation = {...education, schoolName: e.target.value};
+        }else if(type === 'titleStudy'){
+            newEducation = {...education, titleStudy: e.target.value};
+        }else{
+            newEducation = {...education, dateStudy: e.target.value};
+        }
+
+        setEducation(newEducation);
+    }
+
+
+    return(
+        <div>
+            <h2>Educational Experience</h2>
+            <div>
+                <p>School name: </p>
+                <input type="text" onChange={(e) => handleInputChange('schoolName', e)}/>
+            </div>
+            <div>
+                <p>Title of Study: </p>
+                <input type="text" onChange={(e) => handleInputChange('titleStudy', e)}/>
+            </div>
+            <div>
+                <p>Date of study</p>
+                <input type="date" onChange={(e) => handleInputChange('a', e)}/>
+            </div>
+        </div>
+    );
+
+}
+
+
+export default EducationalExperience;
