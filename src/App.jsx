65e88a2b29d6e398b5c0cd4de4bd3fc9d05821ee
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-25 15:45:26 +0200

    feat: add Practical component with all input elements and handler function for input change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import GeneralInfo from './GeneralInfo'
 import EducationalExperience from './Education';
+import Practical from './Practical';
 import './App.css'
 import { useState } from 'react'
 
@@ -7,12 +8,14 @@ function App() {
 
   const [generalInfo, setGeneralInfo] = useState({name:'',email:'',phone:''});
   const [education, setEducation] = useState({schoolName: '', titleStudy: '', dateStudy: ''});
+  const [practice, setPractice] = useState({companyName: '', positionTitle: '', mainRespons: '', dateFrom: '', dateUntil: ''});
 
   return(
     <>
     <div>
       <GeneralInfo info={generalInfo} setInfo={setGeneralInfo}></GeneralInfo>
       <EducationalExperience education={education} setEducation={setEducation}></EducationalExperience>
+      <Practical practice={practice} setPractice={setPractice}></Practical>
     </div>
     </>
   );
---
+++ src/Practical.jsx
@@ -0,0 +1,52 @@
+function Practical({practice, setPractice}) {
+
+    function handleChange(type, e) {
+        let newChange = {};
+
+        if(type === 'compName'){
+            newChange = {...practice, companyName: e.target.value};
+        }else if(type === 'posTitle'){
+            newChange = {...practice, positionTitle: e.target.value};
+        }else if(type === 'responsblt'){
+            newChange = {...practice, mainRespons: e.target.value};
+        }else if(type === 'from'){
+            newChange = {...practice, dateFrom: e.target.value};
+        }else{
+            newChange = {...practice, dateUntil: e.target.value};
+        }
+
+        setPractice(newChange);
+    }
+
+
+    return(
+        <div>
+            <h2>Practical Experiences: </h2>
+            <div>
+                <p>Company Name: </p>
+                <input type="text" onChange={(e) => handleChange('compName', e)}/>
+            </div>
+            <div>
+                <p>Position title: </p>
+                <input type="text" onChange={(e) => handleChange('posTitle', e)} />
+            </div>
+            <div>
+                <p>Main responsibilities: </p>
+                <input type="text" onChange={(e) => handleChange('responsblt', e)} />
+            </div>
+            <div>
+                <p>Worked from: </p>
+                <input type="date" onChange={(e) => handleChange('from', e)} />
+            </div>
+            <div>
+                <p>To:  </p>
+                <input type="date" onChange={(e) => handleChange('to', e)} />
+            </div>
+
+        </div>
+        
+    );
+
+}
+
+export default Practical;
