ac7a5cc96f082e422e703ab6fb6dfdd833bc39c8
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-28 11:27:38 +0200

    fix: The new rendered CV doesn't get changed when the input value is changed on screen

--- src/createCV.jsx
+++ src/createCV.jsx
@@ -1,21 +1,25 @@
+import { useState } from "react";
+
 function CreateCV({cv, setCV, generalInfo, education, practice}) {
+
+    const [alternativeInput, setAlternativeInput] = useState({});
 
     function handleCVCreation() {
         setCV(true);
+        setAlternativeInput({cv, generalInfo, education, practice});
     }
 
     return(
         <>
         <button onClick={handleCVCreation}>Create Your CV!</button>
-        {cv && <CV cv={cv} generalInfo={generalInfo} education={education} practice={practice}></CV>}
+        {cv && <CV cv={alternativeInput.cv} generalInfo={alternativeInput.generalInfo} education={alternativeInput.education} practice={alternativeInput.practice}></CV>}
         </>
     )
 }
 
 function CV({cv, generalInfo, education, practice}) {
-    const ciao = cv;
     return(
-        <div>{ciao}{generalInfo.name}{education.schoolName}{practice.companyName}</div>
+        <div>{cv}{generalInfo.name}{education.schoolName}{practice.companyName}</div>
     )
 }
 
