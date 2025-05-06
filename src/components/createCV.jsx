f4a5627fdf14361cef42833b6dfcac546fb68e69
Vincde <145802178+Vincde@users.noreply.github.com>
2025-05-06 18:49:33 +0200

    Feat: integrate grid display in css insted of using flex

--- src/components/createCV.jsx
+++ src/components/createCV.jsx
@@ -25,26 +25,31 @@ function CV({generalInfo, education, practice, children}) {
     return(
         <div className='showCV'>
             <div className="title">Your CV:</div><br></br>
-            GENERAL INFO:<br></br>
-            <ul>
-               {generalInfo.name && <li>{generalInfo.name}</li>}<br></br>
-                {generalInfo.email && <li>{generalInfo.email}</li>}<br></br>
-                {generalInfo.phone && <li>{generalInfo.email}</li>}<br></br>
-            </ul><br></br>
-            EDUCATION INFO:<br></br>
-            <ul>
-                {education.schoolName && <li>{education.schoolName}</li>}<br></br>
-                {education.titleStudy && <li>{education.titleStudy}</li>}<br></br>
-                {education.dateStudy && <li>{education.dateStudy}</li>}<br></br>
-            </ul><br></br>
-            PRACTICE INFO:<br></br>
-            <ul>
-                {practice.companyName && <li>{practice.companyName}</li>}<br></br>
-                {practice.positionTitle && <li>{practice.positionTitle}</li>}<br></br>
-                {practice.mainRespons && <li>{practice.mainRespons}</li>}<br></br>
-                {practice.dateFrom && <li>{practice.dateFrom}</li>}<br></br>
-                {practice.dateUntil && <li>{practice.dateUntil}</li>}<br></br>
-            </ul>
+            <div className="showCV__mainContainer">  
+                
+                <ul className="showCV__generalInfo">
+                GENERAL INFO:<br></br>
+                   {generalInfo.name && <li>{generalInfo.name}</li>}<br></br>
+                    {generalInfo.email && <li>{generalInfo.email}</li>}<br></br>
+                    {generalInfo.phone && <li>{generalInfo.email}</li>}<br></br>
+                </ul><br></br>
+                
+                <ul className="showCV__education">
+                EDUCATION INFO:<br></br>
+                    {education.schoolName && <li>{education.schoolName}</li>}<br></br>
+                    {education.titleStudy && <li>{education.titleStudy}</li>}<br></br>
+                    {education.dateStudy && <li>{education.dateStudy}</li>}<br></br>
+                </ul>
+                <br></br>
+                <ul className="showCV__practice">
+                PRACTICE INFO:<br></br>
+                    {practice.companyName && <li>{practice.companyName}</li>}<br></br>
+                    {practice.positionTitle && <li>{practice.positionTitle}</li>}<br></br>
+                    {practice.mainRespons && <li>{practice.mainRespons}</li>}<br></br>
+                    {practice.dateFrom && <li>{practice.dateFrom}</li>}<br></br>
+                    {practice.dateUntil && <li>{practice.dateUntil}</li>}<br></br>
+                </ul>
+            </div>
             {children}
         </div>
     )
