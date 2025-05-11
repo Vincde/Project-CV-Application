e79dcb8393bac34bd64d73c459b8d98819518179
Vincde <145802178+Vincde@users.noreply.github.com>
2025-05-11 12:08:09 +0200

    feat: change createCV component to make it show elements correctly

--- src/components/createCV.jsx
+++ src/components/createCV.jsx
@@ -31,20 +31,36 @@ function CV({generalInfo, education, practice, children}) {
                 {generalInfo.email && <li>{generalInfo.email}</li>}<br></br>
                 {generalInfo.phone && <li>{generalInfo.email}</li>}<br></br>
             </ul><br></br>
+            
+            
             EDUCATION INFO:<br></br>
-            <ul>
-                {education.schoolName && <li>{education.schoolName}</li>}<br></br>
-                {education.titleStudy && <li>{education.titleStudy}</li>}<br></br>
-                {education.dateStudy && <li>{education.dateStudy}</li>}<br></br>
-            </ul><br></br>
+            {education.map((element) => {
+                return(
+            <>
+                <ul>
+                {element.schoolName && <li>{element.schoolName}</li>}<br></br>
+                {element.titleStudy && <li>{element.titleStudy}</li>}<br></br>
+                {element.dateStudy && <li>{element.dateStudy}</li>}<br></br>
+                </ul><br></br>
+            </>  
+            )})};
+
+            
             PRACTICE INFO:<br></br>
-            <ul>
-                {practice.companyName && <li>{practice.companyName}</li>}<br></br>
-                {practice.positionTitle && <li>{practice.positionTitle}</li>}<br></br>
-                {practice.mainRespons && <li>{practice.mainRespons}</li>}<br></br>
-                {practice.dateFrom && <li>{practice.dateFrom}</li>}<br></br>
-                {practice.dateUntil && <li>{practice.dateUntil}</li>}<br></br>
-            </ul>
+            {practice.map((element) => {
+                return(
+                <>
+                <ul>
+                    {element.companyName && <li>{element.companyName}</li>}<br></br>
+                    {element.positionTitle && <li>{element.positionTitle}</li>}<br></br>
+                    {element.mainRespons && <li>{element.mainRespons}</li>}<br></br>
+                    {element.dateFrom && <li>{element.dateFrom}</li>}<br></br>
+                    {element.dateUntil && <li>{element.dateUntil}</li>}<br></br>
+                </ul>        
+                </>
+                )
+            })}
+            
             {children}
         </div>
     )
