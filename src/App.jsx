3ed8636e1a3d20bdcbe637a2fb032f588c36501b
Vincde <145802178+Vincde@users.noreply.github.com>
2026-04-19 12:03:07 +0200

    Second implementation of the project from scratch
    All project files and code has been deleted. This is a first commit to push the clean project from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import GeneralInfo from './components/GeneralInfo'
-import EducationalExperience from './components/Education';
-import Practical from './components/Practical';
-import CreateCV from './components/createCV';
-import './App.css'
+
 import { useState } from 'react'
 
 
@@ -10,29 +6,7 @@ import { useState } from 'react'
 
 function App() {
 
-  const [generalInfo, setGeneralInfo] = useState({name:'',email:'',phone:''});
-  const [education, setEducation] = useState([{id:crypto.randomUUID(), schoolName: '', titleStudy: '', dateStudy: ''}]);
-  const [practice, setPractice] = useState([{id:crypto.randomUUID(), companyName: '', positionTitle: '', mainRespons: '', dateFrom: '', dateUntil: ''}]);
-  const [cv, setCV] = useState(false);
-
-  return(
-    <>
-    <div>
-
-      {!cv && <GeneralInfo info={generalInfo} setInfo={setGeneralInfo}></GeneralInfo> }
-      {!cv && <EducationalExperience education={education} setEducation={setEducation}></EducationalExperience> }
-      {!cv && <Practical practice={practice} setPractice={setPractice}></Practical> }
-    </div>
-
-    <CreateCV
-     cv={cv}
-     setCV={setCV}
-     generalInfo={generalInfo}
-     education={education}
-     practice={practice}>
-     </CreateCV>
-    </>
-  );
+  
 }
 
 
