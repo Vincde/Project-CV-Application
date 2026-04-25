da15b575da5be876955e600aaff0e2ab59f75069
Vincde <145802178+Vincde@users.noreply.github.com>
2026-04-25 12:11:41 +0200

    Add icons and first section of the final cv component

--- src/components/CompleteCV.jsx
+++ src/components/CompleteCV.jsx
@@ -1,3 +1,8 @@
+import person from './../../public/person.jpeg';
+import personName from './../../public/person_icon.png';
+import personEmail from './../../public/mail_icon.png';
+import personPhone from './../../public/phone_icon.png';
+
 export default function CompleteCV({infos, setInfos}) {
 
     function handlePreviousPage(){
@@ -9,9 +14,30 @@ export default function CompleteCV({infos, setInfos}) {
 
     return(
         <>
-            <div>
-                <h1>CV</h1>
-                <h2>{infos[0].name}</h2>
+            <div className="cv-container">
+                <section>
+                    <h1>About Me</h1>
+                    <img src={person} alt="person described in the curriculum" />
+                    <div>
+                        <img src={personName} alt="person's name" />
+                        <span>{infos[0].name}</span>
+                    </div>
+                    <div>
+                        <img src={personEmail} alt="person's mail" />
+                        <span>{infos[0].email}</span>
+                    </div>
+                    <div>
+                        <img src={personPhone} alt="person's phone" />
+                        <span>{infos[0].phone}</span>
+                    </div>
+                </section>
+                <section>
+                    <h2>Educational Experience</h2>
+                    <p>School Name: {}</p>
+                    <p>Title Of Study</p>
+                    <p>Date Of Study</p>
+                </section>
+                <section></section>
             </div>
         
         <button onClick={handlePreviousPage}>Edit</button>
