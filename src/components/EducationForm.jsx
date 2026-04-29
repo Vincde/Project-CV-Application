e116c3c60c9f0c1f4d2c41cfab232a7727b98b72
Vincde <145802178+Vincde@users.noreply.github.com>
2026-04-29 14:05:09 +0200

    Add basic hthml for EducationForm component

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,3 +1,22 @@
 export default function EducationForm(){
+ 
     
+
+
+
+    return(
+        <article>
+            <form onSubmit={(e) => e.preventDefault()}>
+
+                <label htmlFor="schoolName">School Name</label>
+                <input type="text" id="schoolName"/>
+
+                <label htmlFor="titleOfStudy">Title Of Study</label>
+                <input type="text" id="titleOfStudy" />
+
+                <label htmlFor="dateOfStudy">Date of Study</label>
+                <input type="date" id="dateOfStudy"/>
+            </form>
+        </article>
+    );
 }
