7078270ac5b71adb28f46b37e61622f84eb4d878
Vincde <145802178+Vincde@users.noreply.github.com>
2026-04-29 14:10:32 +0200

    Add basic html structure for PracticeForm

--- src/components/PracticeForm.jsx
+++ src/components/PracticeForm.jsx
@@ -1,3 +1,28 @@
 export default function PracticeForm(){
    
+
+
+
+    return(
+        <article>
+            <form onSubmit={(e) => e.preventDefault()}>
+
+                <label htmlFor="companyName">Company Name</label>
+                <input type="text" id="companyName"/>
+
+                <label htmlFor="positionTitle">Position Title</label>
+                <input type="text" id="positionTitle" />
+
+                <label htmlFor="mainResponsibilities">Main Responsibilities</label>
+                <textarea id="mainResponsibilities"></textarea>
+            
+                <label htmlFor="dateStart">Date when you started</label>
+                <input type="date" id="dateStart"/>
+                
+                <label htmlFor="dateEnd">Date when you finished</label>
+                <input type="date" id="dateEnd"/>
+            
+            </form>
+        </article>
+    );
 }
