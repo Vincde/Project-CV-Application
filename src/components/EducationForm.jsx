2166840af78f6d091b6de1e78e33fa1313ec56fe
Vincde <145802178+Vincde@users.noreply.github.com>
2026-04-30 22:42:20 +0200

    Fix update issue in EducationInfo component

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -23,6 +23,7 @@ export default function EducationForm({sendForm}){
                     editedObj.push({...el});
                 }
             });
+            setEducationInfo([...editedObj]);
             setInputs((prev) => ({...prev, id: -1}));
         }
 
