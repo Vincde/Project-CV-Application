75a5e2890c2d66b389f00527410e7a348891e49a
Vincde <145802178+Vincde@users.noreply.github.com>
2026-05-10 21:51:36 +0200

    Add icons to completeCV component + initial styling

--- src/components/CompleteCV.jsx
+++ src/components/CompleteCV.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import nameIcon from './../../public/person_icon.png';
+import mailIcon from './../../public/mail_icon.png';
+import phoneIcon from './../../public/phone_icon.png';
+import './../styles/completeCV.css';
 
 export default function CompleteCV({generalInfo, educationInfo, practiceInfo, handleEdit}) {
     const image = generalInfo.image;
@@ -15,15 +19,35 @@ export default function CompleteCV({generalInfo, educationInfo, practiceInfo, ha
   }, [image]);
   
   return(
-    <>
-    <div className="completeCV-generalInfo">
-      <p>General Information</p>
+    <div className="completeCV-container">
+      
+      <button onClick={handleEdit}>edit</button>
+
+      <div className="completeCV-generalInfo">
+        <p>General Information</p>
+
+        <img src={anteprima} alt="" />
+
+        <div>
+          <img src={nameIcon} alt="" />
+          <p>{generalInfo.name}</p>
+        </div>
+
+        <div>
+          <img src={mailIcon} alt="" />
+          <p>{generalInfo.email}</p>
+        </div>
+
+        <div>
+          <img src={phoneIcon} alt="" />
+          <p>{generalInfo.phone}</p>
+        </div>
+
+        
+      </div>
+    
       {educationInfo.schoolName}
       {practiceInfo.companyName}
-      <p>Profile Pic</p>
-      <img src={anteprima} alt="" />
     </div>
-    <button onClick={handleEdit}>edit</button>
-    </>
   );
 }
