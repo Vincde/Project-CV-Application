036aa5d1bade21800d3c114c7d8547aadf8e6de6
Vincde <145802178+Vincde@users.noreply.github.com>
2026-05-11 18:15:21 +0200

    Add css styling for education and practice child content

--- src/components/CompleteCV.jsx
+++ src/components/CompleteCV.jsx
@@ -51,14 +51,42 @@ export default function CompleteCV({generalInfo, educationInfo, practiceInfo, ha
 
       <div className="completeCV-educationInfo">
         <h2>Education Info</h2>
+
+        <div className="educationInfo-containerBox">
+          {
+            educationInfo.map((el) => {
+              return(
+                <div key={el.id} className="educationInfo-singleBox">
+                  <p>School Name: {el.schoolName}</p>
+                  <p>Title Of Study: {el.titleOfStudy}</p>
+                  <p>Date of Study: {el.dateOfStudy}</p>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
 
 
       <div className="completeCV-practiceInfo">
         <h2>Practice Info</h2>
 
-        {educationInfo.schoolName}
-      {practiceInfo.companyName}
+
+        <div className="practiceInfo-containerBox">
+          {
+            practiceInfo.map((el) => {
+              return(
+                <div key={el.id} className="practiceInfo-singleBox">
+                  <p>Company Name: {el.companyName}</p>
+                  <p>Position Title: {el.positionTitle}</p>
+                  <p>Main Responsibilities: {el.mainResponsibilities}</p>
+                  <p>Started: {el.dateStart}</p>
+                  <p>Finished: {el.dateEnd}</p>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     
       
